b76c40bb7fc69a5af9ac98413020e77bf0927261
huangtianquan85 <74945668+huangtianquan85@users.noreply.github.com>
2021-11-17 15:05:44 +0800

    optimize

--- server/main.go
+++ server/main.go
@@ -12,51 +12,50 @@ import (
 	"strconv"
 )
 
-var offset int
-var privateKey *rsa.PrivateKey
+func makeHandler(offset int, privateKey *rsa.PrivateKey) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// read body
+		body := make([]byte, r.ContentLength)
+		r.Body.Read(body)
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	// read body
-	body := make([]byte, r.ContentLength)
-	r.Body.Read(body)
+		// base64 decode
+		cipherText, err := base64.StdEncoding.DecodeString(string(body))
+		if err != nil {
+			fmt.Fprintln(w, "base64 decode error")
+			return
+		}
 
-	// base64 decode
-	cipherText, err := base64.StdEncoding.DecodeString(string(body))
-	if err != nil {
-		fmt.Fprintln(w, "base64 decode error")
-		return
+		// decrypt
+		rawData, err := rsa.DecryptPKCS1v15(rand.Reader, privateKey, cipherText)
+		if err != nil {
+			fmt.Fprintln(w, "decrypt error")
+			return
+		}
+
+		// read the random number from client
+		num, err := strconv.Atoi(string(rawData))
+		if err != nil {
+			fmt.Fprintln(w, "read number error")
+			return
+		}
+
+		// return random number + offset to client
+		num += offset
+		fmt.Fprintln(w, strconv.Itoa(num))
 	}
-
-	// decrypt
-	rawData, err := rsa.DecryptPKCS1v15(rand.Reader, privateKey, cipherText)
-	if err != nil {
-		fmt.Fprintln(w, "decrypt error")
-		return
-	}
-
-	// read the random number from client
-	num, err := strconv.Atoi(string(rawData))
-	if err != nil {
-		fmt.Fprintln(w, "read number error")
-		return
-	}
-
-	// return random number + offset to client
-	num += offset
-	fmt.Fprintln(w, strconv.Itoa(num))
 }
 
-func load() error {
+func load() (int, *rsa.PrivateKey, error) {
 	var err error
-	offset, err = strconv.Atoi(os.Getenv("OFFSET"))
+	offset, err := strconv.Atoi(os.Getenv("OFFSET"))
 	if err != nil {
-		return fmt.Errorf("read OFFSET error %v", err)
+		return offset, nil, fmt.Errorf("read OFFSET error %v", err)
 	}
 
 	// load key bytes
 	keyFile, err := os.Open("app.pem")
 	if err != nil {
-		return fmt.Errorf("open key file error %v", err)
+		return offset, nil, fmt.Errorf("open key file error %v", err)
 	}
 	defer keyFile.Close()
 	st, _ := keyFile.Stat()
@@ -66,25 +65,25 @@ func load() error {
 	// load key
 	block, _ := pem.Decode(keyBytes)
 	if block == nil {
-		return fmt.Errorf("decode private key error %v", err)
+		return offset, nil, fmt.Errorf("decode private key error %v", err)
 	}
 
-	privateKey, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
 	if err != nil {
-		return fmt.Errorf("parse private key error %v", err)
+		return offset, privateKey, fmt.Errorf("parse private key error %v", err)
 	}
 
-	return nil
+	return offset, privateKey, nil
 }
 
 func main() {
-	err := load()
+	offset, privateKey, err := load()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
 	// start http server
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", makeHandler(offset, privateKey))
 	http.ListenAndServe("0.0.0.0:8080", nil)
 }
